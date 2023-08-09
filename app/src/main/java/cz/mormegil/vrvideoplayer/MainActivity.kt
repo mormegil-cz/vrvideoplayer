5aaf8aa52ecfabd7d676530b5f26dcc7aee11e7b
Mormegil <mormegil@centrum.cz>
2023-08-09 17:44:55 +0200

    WIP: Separate initial chooser activity
    MainActivity now _always_ loads with an available video

--- app/src/main/java/cz/mormegil/vrvideoplayer/MainActivity.kt
+++ app/src/main/java/cz/mormegil/vrvideoplayer/MainActivity.kt
@@ -1,13 +1,9 @@
 package cz.mormegil.vrvideoplayer
 
-import android.content.Intent
-import android.net.Uri
 import android.opengl.GLSurfaceView
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import androidx.activity.result.PickVisualMediaRequest
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import cz.mormegil.vrvideoplayer.databinding.ActivityMainBinding
 import javax.microedition.khronos.egl.EGLConfig
@@ -25,72 +21,39 @@ class MainActivity : AppCompatActivity() {
 
     private var nativeApp: Long = 0
 
-    private var ready = false
-    private var resume = false
-
-    private val videoGalleryChooser =
-        registerForActivityResult((ActivityResultContracts.PickVisualMedia()), ::initWithVideo)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         Log.d(TAG, "onCreate()")
 
+        val videoUri = intent.data
+        if (videoUri == null) {
+            // ? should not happen
+            Log.w(TAG, "No URI for intent")
+            finish()
+            return
+        }
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         glView = binding.surfaceview
+        glView.setEGLContextClientVersion(2)
+        val renderer = Renderer()
+        glView.setRenderer(renderer)
+        glView.renderMode = GLSurfaceView.RENDERMODE_CONTINUOUSLY
         glView.setOnClickListener {
             val toast = Toast.makeText(
                 this,
                 "This demo combines Java UI and native EGL + OpenGL renderer",
                 Toast.LENGTH_LONG
             )
-            toast.show();
+            toast.show()
         }
-
-        when (intent.action) {
-            Intent.ACTION_VIEW -> {
-                val viewUri = intent.data;
-                if (viewUri != null) {
-                    initWithVideo(viewUri);
-                } else {
-                    chooseVideoFromGallery();
-                }
-            }
-
-            // Intent.ACTION_MAIN ->
-            else -> {
-                chooseVideoFromGallery();
-            }
-        }
-    }
-
-    private fun chooseVideoFromGallery() {
-        videoGalleryChooser.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.VideoOnly))
-    }
-
-    private fun initWithVideo(videoUri: Uri?) {
-        if (videoUri == null) {
-            // bah!
-            finish()
-        }
-
-        glView.setEGLContextClientVersion(2)
-        val renderer = Renderer()
-        glView.setRenderer(renderer)
-        glView.renderMode = GLSurfaceView.RENDERMODE_CONTINUOUSLY
 
         videoTexturePlayer = VideoTexturePlayer(this, videoUri!!)
 
         nativeApp = NativeLibrary.nativeInit(this, assets, videoTexturePlayer)
-
-        ready = true
-
-        if (resume) {
-            resume = false
-            doResume()
-        }
     }
 
     private fun doResume() {
@@ -102,20 +65,12 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         Log.d(TAG, "onResume()")
-        if (!ready) {
-            resume = true
-            return
-        }
         doResume()
     }
 
     override fun onPause() {
         super.onPause()
         Log.d(TAG, "onPause()")
-        if (!ready) {
-            resume = false
-            return
-        }
         videoTexturePlayer.onPause()
         NativeLibrary.nativeOnPause(nativeApp)
         glView.onPause()
@@ -123,9 +78,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-        if (!ready) {
-            return
-        }
         Log.d(TAG, "onDestroy()")
         NativeLibrary.nativeOnDestroy(nativeApp)
         nativeApp = 0
@@ -134,16 +86,16 @@ class MainActivity : AppCompatActivity() {
 
     private inner class Renderer : GLSurfaceView.Renderer {
         override fun onSurfaceCreated(gl10: GL10?, config: EGLConfig?) {
-            NativeLibrary.nativeOnSurfaceCreated(nativeApp);
+            NativeLibrary.nativeOnSurfaceCreated(nativeApp)
         }
 
         override fun onSurfaceChanged(gl10: GL10?, width: Int, height: Int) {
-            NativeLibrary.nativeSetScreenParams(nativeApp, width, height);
+            NativeLibrary.nativeSetScreenParams(nativeApp, width, height)
         }
 
         override fun onDrawFrame(gl10: GL10?) {
             videoTexturePlayer.updateIfNeeded()
-            NativeLibrary.nativeDrawFrame(nativeApp);
+            NativeLibrary.nativeDrawFrame(nativeApp)
         }
     }
 }
---
+++ app/src/main/java/cz/mormegil/vrvideoplayer/StartupActivity.kt
@@ -0,0 +1,58 @@
+package cz.mormegil.vrvideoplayer
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+
+class StartupActivity : ComponentActivity() {
+    companion object {
+        private const val TAG = "VRVideoPlayerS"
+    }
+
+    private val videoGalleryChooser =
+        registerForActivityResult((ActivityResultContracts.PickVisualMedia()), ::initWithVideo)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Log.d(TAG, "onCreate()")
+
+        when (intent.action) {
+            Intent.ACTION_VIEW -> {
+                val viewUri = intent.data
+                if (viewUri != null) {
+                    initWithVideo(viewUri)
+                } else {
+                    chooseVideoFromGallery()
+                }
+            }
+
+            // Intent.ACTION_MAIN ->
+            else -> {
+                chooseVideoFromGallery()
+            }
+        }
+    }
+
+    private fun chooseVideoFromGallery() {
+        videoGalleryChooser.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.VideoOnly))
+    }
+
+    private fun initWithVideo(videoUri: Uri?) {
+        if (videoUri == null) {
+            // bah!
+            Log.d(TAG, "No video chosen, aborting")
+            finish()
+        }
+        Log.d(TAG, "initWithVideo: $videoUri")
+
+        val intent = Intent(this, MainActivity::class.java)
+        intent.data = videoUri
+        startActivity(intent)
+        finish()
+    }
+}
