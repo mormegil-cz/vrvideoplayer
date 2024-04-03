cb21c289bd9516bb68784a78db0eb33ad0d63e62
mormegil <mormegil@centrum.cz>
2024-04-03 13:04:16 +0200

    WIP: First piece of progress bar rendering

--- app/src/main/cpp/VRGuiProgressBar.cpp
+++ app/src/main/cpp/VRGuiProgressBar.cpp
@@ -2,6 +2,8 @@
 
 #include <GLES2/gl2.h>
 
+static constexpr GLubyte line2DData[] = {0, 1};
+
 VRGuiProgressBar::VRGuiProgressBar(float xCenter, float yCenter, float width, float height)
         : xCenter(xCenter),
           yCenter(yCenter),
@@ -10,8 +12,15 @@ VRGuiProgressBar::VRGuiProgressBar(float xCenter, float yCenter, float width, fl
           progress(0) {
 }
 
-void VRGuiProgressBar::render(GLint programParamPosition) const {
+void VRGuiProgressBar::render(GLint program2DParamPosition) const {
+    std::array<float, 6> progressLineCoords = {xCenter - 0.5f * width, yCenter, 0.5f,
+                                               xCenter + width * (progress - 0.5f), yCenter, 0.5f};
 
+    glEnableVertexAttribArray(program2DParamPosition);
+    glVertexAttribPointer(program2DParamPosition, 3, GL_FLOAT, GL_FALSE, 0,
+                          progressLineCoords.data());
+
+    glDrawElements(GL_LINES, 2, GL_UNSIGNED_BYTE, line2DData);
 }
 
 void VRGuiProgressBar::setProgress(float progress) {
